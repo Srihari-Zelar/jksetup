36ca222be6f5e308f5c99e3e75c90f808169cc5b
Srihari <Srihari@zelarsoft.com>
2021-09-09 13:17:36 +0530

    update

--- jenkins.sh
+++ jenkins.sh
@@ -2,22 +2,20 @@
 
 
 apt update -y
-stat $?
 
 wget -q -O - https://pkg.jenkins.io/debian/jenkins.io.key | sudo apt-key add -
-stat $?
 
 sudo sh -c 'echo deb https://pkg.jenkins.io/debian binary/ > /etc/apt/sources.list.d/jenkins.list'
-stat $?
 
-sudo apt-get update
-stat $?
+sudo apt install openjdk-8-jre-headless -y
 
-sudo apt install openjdk-8-jre
-stat $?
+sudo apt install openjdk-8-jdk-headless -y
 
-sudo apt-get install jenkins
-stat $?
+sudo apt install maven -y
+
+sudo apt-get update -y
+
+sudo apt-get install jenkins -y
 
 systemctl enable jenkins
 
