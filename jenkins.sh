a00749d64e59facff87cd04dcedf1b19ff4458a9
Srihari <Srihari@zelarsoft.com>
2021-09-09 12:48:13 +0530

    update

---
+++ jenkins.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+
+
+apt update -y
+
+wget -q -O - https://pkg.jenkins.io/debian/jenkins.io.key | sudo apt-key add -
+
+sudo sh -c 'echo deb https://pkg.jenkins.io/debian binary/ > /etc/apt/sources.list.d/jenkins.list'
+
+sudo apt-get update
+
+sudo apt-get install jenkins
